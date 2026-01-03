481071d86a89a57f25e000ad9ef7b41716d25a5b
Carlos Gamino <carlos.alfredo55@gmail.com>
2026-01-02 20:53:26 -0600

    store: Add test for create products

--- ecommerce-storefront/tests/example.spec.ts
+++ ecommerce-storefront/tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test.beforeEach(async ({ page }) => {
   // Runs before each test and signs in each page.
@@ -27,17 +28,18 @@ test('testCreateProduct', async ({ page }) => {
   await expect(page.getByRole('textbox', { name: 'Description' })).toBeEmpty();
   await expect(page.getByRole('textbox', { name: 'Image URL' })).toBeEmpty();
   await page.getByRole('textbox', { name: 'Name' }).click();
-  await page.getByRole('textbox', { name: 'Name' }).fill('Custom Product');
+  const productName = faker.commerce.productName();
+  await page.getByRole('textbox', { name: 'Name' }).fill(productName);
   await page.locator('input[name="price"]').click();
-  await page.locator('input[name="price"]').fill('500.01');
+  await page.locator('input[name="price"]').fill(faker.commerce.price({ min: 1, max: 1000 }));
   await page.getByRole('spinbutton', { name: 'Stock' }).click();
-  await page.getByRole('spinbutton', { name: 'Stock' }).fill('20');
+  await page.getByRole('spinbutton', { name: 'Stock' }).fill(faker.number.int({ min: 1, max: 1000 }).toString());
   await page.getByRole('combobox', { name: 'Category' }).click();
   await page.getByLabel('Books').getByText('Books').click();
   await page.getByRole('textbox', { name: 'Description' }).click();
-  await page.getByRole('textbox', { name: 'Description' }).fill('Custom description');
+  await page.getByRole('textbox', { name: 'Description' }).fill(faker.commerce.productDescription());
   await page.getByRole('textbox', { name: 'Image URL' }).click();
   await page.getByRole('textbox', { name: 'Image URL' }).fill('https://placehold.co/600x400');
   await page.getByRole('button', { name: 'Save' }).click();
-  await expect(page.locator('tbody')).toContainText('Custom Product');
+  await expect(page.locator('tbody')).toContainText(productName);
 });
