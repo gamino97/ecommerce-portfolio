7a8330ea97e198fdc8ea5beb8efc7875234b5229
Carlos Gamino <carlos.alfredo55@gmail.com>
2026-05-20 14:51:18 -0600

    Add docker on backend

--- ecommerce-backend/app/config.py
+++ ecommerce-backend/app/config.py
@@ -7,7 +7,9 @@ class Settings(BaseSettings):
     database_url: str
     secret: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=".env", secrets_dir="/run/secrets"
+    )
 
 
 @lru_cache
