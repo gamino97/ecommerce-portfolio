583ce479f993d3e34c8217ea528f26cebf0ab197
Carlos Gamino <carlos.alfredo55@gmail.com>
2026-05-25 17:56:16 -0600

    Improve healthcheck in Dockerfiles

--- ecommerce-backend/migrate.sh
+++ ecommerce-backend/migrate.sh
@@ -1,2 +1,3 @@
-docker exec -it ecommerce-portfolio-ecommerce-portfolio-backend-1 uv run alembic upgrade head
-docker exec -it ecommerce-portfolio-ecommerce-portfolio-backend-1 uv run seed.py
+# Do not use % signs as your password.
+sudo docker exec -it ecommerce-portfolio-ecommerce-portfolio-backend-1 uv run alembic upgrade head
+sudo docker exec -it ecommerce-portfolio-ecommerce-portfolio-backend-1 uv run seed.py
